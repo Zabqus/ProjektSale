4b8e1d43423fc7feed46e0b88a2f9c89b603efed
WiktorZabek <zabek.wiktor04@gmail.com>
2025-05-21 22:37:18 +0200

    Adding the necessary tools to the conference room booking project

---
+++ src/main/java/com/example/projektsale/ProjektSaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektSaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektSaleApplication.class, args);
+    }
+
+}
